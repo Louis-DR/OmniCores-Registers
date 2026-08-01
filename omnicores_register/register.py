330a30b73c527732f6275e9a09e2ad9ddd9d2293
Louis Duret-Robert <louisduret@gmail.com>
2026-08-02 01:14:18 +0200

    |FW][feat] Generate C headers with macros and structs

--- omnicores_register/register.py
+++ omnicores_register/register.py
@@ -24,13 +24,14 @@ class Register:
       software_access : SoftwareAccessType = SoftwareAccessType.READ_WRITE,
       hardware_access : HardwareAccessType = HardwareAccessType.READ_ONLY,
     ):
-    self.name            = name
-    self.width           = width
-    self.offset          = offset
-    self.address         = None
-    self.reset_value     = reset_value
-    self.software_access = software_access
-    self.hardware_access = hardware_access
+    self.name              = name
+    self.width             = width
+    self.offset            = offset
+    self.address           = None
+    self.sw_struct_padding = 0
+    self.reset_value       = reset_value
+    self.software_access   = software_access
+    self.hardware_access   = hardware_access
 
   def is_software_readable(self) -> bool:
     return self.software_access in [SoftwareAccessType.READ_ONLY, SoftwareAccessType.READ_WRITE]
