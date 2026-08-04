a184bb7d6b91387d5bb3498c633809a1d48a8430
Louis Duret-Robert <louisduret@gmail.com>
2026-08-04 14:59:31 +0200

    [API][feat] Register fields

--- omnicores_register/elaborate.py
+++ omnicores_register/elaborate.py
@@ -10,6 +10,17 @@
 
 
 
+from omnicores_register.enums import (
+  SoftwareAccessType,
+  HardwareAccessType,
+  register_default_software_access,
+  register_default_hardware_access,
+  field_default_software_access,
+  field_default_hardware_access,
+)
+
+
+
 def elaborate(self):
   """Elaborate the data structure after configuration and before generation."""
   # Address offset of each register
@@ -22,9 +33,70 @@ def elaborate(self):
       running_address  = register.offset
     running_address += 4
 
-  # Padding before each register for firmware struct header
+  # Bit offset and padding of each register field
+  for register in self.registers:
+    if register.fields:
+      running_offset = 0
+      for field in register.fields:
+        if field.offset is None:
+          field.offset = running_offset
+        else:
+          running_offset = field.offset
+        running_offset += field.width
+
+  # Access policy between fields and registers
+  for register in self.registers:
+    # If the register has fields, for fields that do have a specified access policy,
+    # they take the policy of their register, or the default if the register also
+    # doesn't have an explicit policy.
+    if register.fields:
+      for field in register.fields:
+        field.software_access = field.software_access or register.software_access or field_default_software_access
+        field.hardware_access = field.hardware_access or register.hardware_access or field_default_hardware_access
+    register.software_access = register.software_access or register_default_software_access
+    register.hardware_access = register.hardware_access or register_default_hardware_access
+    # Upgrade register access policies according to its fields
+    if register.fields:
+      for field in register.fields:
+        if field.is_software_writable():
+          register.software_access = {
+            SoftwareAccessType.NONE       : SoftwareAccessType.WRITE_ONLY,
+            SoftwareAccessType.READ_ONLY  : SoftwareAccessType.READ_WRITE,
+            SoftwareAccessType.WRITE_ONLY : SoftwareAccessType.WRITE_ONLY,
+            SoftwareAccessType.READ_WRITE : SoftwareAccessType.READ_WRITE,
+          }[register.software_access]
+        if field.is_software_readable():
+          register.software_access = {
+            SoftwareAccessType.NONE       : SoftwareAccessType.READ_ONLY,
+            SoftwareAccessType.READ_ONLY  : SoftwareAccessType.READ_ONLY,
+            SoftwareAccessType.WRITE_ONLY : SoftwareAccessType.READ_WRITE,
+            SoftwareAccessType.READ_WRITE : SoftwareAccessType.READ_WRITE,
+          }[register.software_access]
+        if field.is_hardware_writable():
+          register.hardware_access = {
+            HardwareAccessType.NONE       : HardwareAccessType.WRITE_ONLY,
+            HardwareAccessType.READ_ONLY  : HardwareAccessType.READ_WRITE,
+            HardwareAccessType.WRITE_ONLY : HardwareAccessType.WRITE_ONLY,
+            HardwareAccessType.READ_WRITE : HardwareAccessType.READ_WRITE,
+          }[register.hardware_access]
+        if field.is_hardware_readable():
+          register.hardware_access = {
+            HardwareAccessType.NONE       : HardwareAccessType.READ_ONLY,
+            HardwareAccessType.READ_ONLY  : HardwareAccessType.READ_ONLY,
+            HardwareAccessType.WRITE_ONLY : HardwareAccessType.READ_WRITE,
+            HardwareAccessType.READ_WRITE : HardwareAccessType.READ_WRITE,
+          }[register.hardware_access]
+
+  # Padding before each register and fields for firmware struct header
   previous_address = -4
   for register in self.registers:
     if register.is_software_readable() or register.is_software_writable():
       register.sw_struct_padding = (register.address - previous_address - 4) // 4
       previous_address = register.address
+    if register.fields:
+      previous_offset = 0
+      for field in register.fields:
+        if field.is_software_readable() or field.is_software_writable():
+          field.sw_struct_padding = field.offset - previous_offset
+          previous_offset = field.offset + field.width
+      register.sw_struct_fields_padding = 32 - previous_offset
--- omnicores_register/field.py
+++ omnicores_register/field.py
@@ -4,41 +4,35 @@
 # ║ Website:     louis-dr.github.io                                           ║
 # ║ License:     MIT License                                                  ║
 # ╟───────────────────────────────────────────────────────────────────────────╢
-# ║ Description: Register object class.                                       ║
+# ║ Description: Register field object class.                                 ║
 # ╚═══════════════════════════════════════════════════════════════════════════╝
 
 
 
 from typing import Optional
 from omnicores_register.enums import SoftwareAccessType, HardwareAccessType
-from omnicores_register.field import Field
 
 
 
-class Register:
+class Field:
   def __init__(
       self,
       name            : str,
-      width           : int                          = 32,
+      width           : int                          = 1,
       offset          : Optional[int]                = None,
       reset_value     : Optional[int]                = 0,
       software_access : Optional[SoftwareAccessType] = None, # Default defined in elaboration
       hardware_access : Optional[HardwareAccessType] = None, # Default defined in elaboration
-      fields          : Optional[list[Field]]        = None,
     ):
     self.name              = name
     self.width             = width
     self.offset            = offset
-    self.address           = None
     self.reset_value       = reset_value
     self.software_access   = software_access
     self.hardware_access   = hardware_access
-    self.fields            = fields or []
 
-    # Padding with previous register
+    # Padding with previous field
     self.sw_struct_padding = 0
-    # Padding after the last field to fill the register width
-    self.sw_struct_fields_padding = 0
 
   def is_software_readable(self) -> bool:
     return self.software_access in [SoftwareAccessType.READ_ONLY, SoftwareAccessType.READ_WRITE]
@@ -57,6 +51,3 @@ class Register:
 
   def is_hardware_accessible(self) -> bool:
     return self.is_hardware_readable() or self.is_hardware_writable()
-
-  def add_field(self, field:Field):
-    self.fields.append(field)
